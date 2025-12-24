373c0bd1751ab9e8ce19296ead8a60c2574ed4e6
chadrack-123 <chadrackmulamba2@gmail.com>
2025-12-24 20:21:51 +0200

    Configure for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/chadrack_portfolio' : '',
 };
 
 export default nextConfig;
